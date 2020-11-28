8ecf41e6137323a41a7128dca4e7eaa33acc46a7
Mohit <mohitsingh11082002@gmail.com>
2020-11-28 15:49:45 +0530

    Change AlertDialog of loading indicator

--- app/src/main/java/com/example/userapp/MyApp.java
+++ app/src/main/java/com/example/userapp/MyApp.java
@@ -1,10 +1,15 @@
 package com.example.userapp;
 
 import android.app.Application;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -15,7 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MyApp extends Application {
 
     public FirebaseFirestore db;
-    public AlertDialog dialog;
+    public Dialog dialog;
     public ConnectivityManager connectivityManager;
 
     @Override
@@ -30,11 +35,29 @@ public class MyApp extends Application {
     }
 
     public void showLoadingDialog(Context context) {
+
         CircularLoadingLottieBinding binding = CircularLoadingLottieBinding.inflate(LayoutInflater.from(context));
-        dialog = new AlertDialog.Builder(context, R.style.CustomAlertDialog)
-                .setCancelable(false)
-                .setView(binding.getRoot())
-                .show();
+
+        if(dialog != null && dialog.isShowing())
+            return;
+
+        dialog = new Dialog(context, R.style.LoaderStyle);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(binding.getRoot());
+
+        if(dialog.getWindow() == null)
+            return;
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.show();
+
+//        CircularLoadingLottieBinding binding = CircularLoadingLottieBinding.inflate(LayoutInflater.from(context));
+//        dialog = new AlertDialog.Builder(context, R.style.CustomAlertDialog)
+//                .setCancelable(false)
+//                .setView(binding.getRoot())
+//                .show();
+
     }
 
     public void hideLoadingDialog() {
